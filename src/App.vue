e5eab5b53e08275064a61ffb74b42f4b49f90921
Todd Nichols <nivram80@gmail.com>
2018-06-26 21:44:04 -0500

    create basic unstyled dropdown component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Dropdown
+      :label="'Best Royals Player'"
+      :options="players"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dropdown from './components/Dropdown.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Dropdown
+  },
+  data () {
+    return {
+      players: [
+        'George Brett',
+        'Willie Wilson',
+        'Mike Sweeney',
+        'Salvador Perez',
+        'Amos Otis'
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="dropdown">
+        <div class="input">{{label}}</div>
+        <div class="options">
+            <div
+                class="option"
+                v-for="option in options"
+                :key="option"
+            >
+                {{option}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Dropdown",
+    props: {
+      label: {
+        type: String,
+        required: true
+      },
+      options: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
